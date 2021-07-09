c820d75b7e3c55be4f3eee3e222980aca6a6f467
Yigithan Guven <demirejderha@gmail.com>
2021-07-09 21:56:39 +0300

    GameSpace_Fix

--- HyperCasualGame/Assets/Scripts/PlayerController.cs
+++ HyperCasualGame/Assets/Scripts/PlayerController.cs
@@ -25,6 +25,7 @@ public class PlayerController : MonoBehaviour
     {
         foodSpawner = GameObject.FindGameObjectWithTag("FoodSpawner").GetComponent<FoodSpawner>();
         _swerveInputSystem = GetComponent<SwerveInputSystem>();
+
     }
 
     // Update is called once per frame
